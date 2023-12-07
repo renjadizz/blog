628473dfe40280071328ca60a675f51a44bdc772
Murat <renja.dizz@gmail.com>
2023-12-07 23:11:27 +0900

    add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <p>ReadWorld Blog</p>
+      </div>
+      <div className="header__right">
+        <button>Log IN</button>
+      </div>
+    </div>
+  );
+}
+export default Header;
